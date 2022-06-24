4e43c3ce17702260c11e209034a3aede3684e9bc
Rocky <praneethbhargav44@gmail.com>
2022-06-25 01:44:42 +0530

    The First Commit

---
+++ app/src/main/java/com/example/android/agritech/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.android.agritech.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryColor = Color(0xFF76ff03)
+val primaryLightColor = Color(0xFFb0ff57)
+val primaryDarkColor = Color(0xFF32cb00)
+val secondaryColor=Color(0xFFf57f17)
+val secondaryLightColor = Color(0xFFffb04c)
+val secondaryDarkColor=Color(0xFFbc5100)
+val RedErrorDark = Color(0xFFB00020)
+val RedErrorLight = Color(0xFFEF5350)
+val white=Color(0xFFFFFFFF)
+val black=Color(0xFF28272B)
---
+++ app/src/main/java/com/example/android/agritech/ui/theme/Theme.kt
@@ -0,0 +1,63 @@
+package com.example.android.agritech.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary= primaryColor,
+    primaryVariant= primaryDarkColor,
+    secondary= secondaryColor,
+    secondaryVariant= secondaryLightColor,
+    background= black,
+    surface= primaryLightColor,
+    error= RedErrorDark,
+    onPrimary= white,
+    onSecondary= white,
+    onBackground= white,
+    onSurface= white,
+    onError= RedErrorLight,
+)
+
+private val LightColorPalette = lightColors(
+    primary= primaryColor,
+    primaryVariant= primaryLightColor,
+    secondary= secondaryColor,
+    secondaryVariant= secondaryDarkColor,
+    background= primaryLightColor,
+    surface= primaryLightColor,
+    error= RedErrorDark,
+    onPrimary= white,
+    onSecondary= white,
+    onBackground= white,
+    onSurface= white,
+    onError= RedErrorLight,
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun AgritechTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
